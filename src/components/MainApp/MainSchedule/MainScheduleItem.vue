9cc91744f7e7c10e1f7fcd04f920e25e503775e5
Anar <aanar1k14@gmail.com>
2023-12-07 18:42:29 +0200

    updates

--- src/components/MainApp/MainSchedule/MainScheduleItem.vue
+++ src/components/MainApp/MainSchedule/MainScheduleItem.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { Star } from '@element-plus/icons-vue';
+
 </script>
 
 <template>
   <li
-    class="flex flex-col md:flex-row items-center md:items-stretch gap-2 md:gap-20 pb-3 last:pb-0 group text-center md:text-left">
-    <h4 class="text-xl md:text-2xl text-primary">09:00</h4>
+    class="group flex gap-4 md:gap-10 pb-3 last:pb-0">
+    <h4 class="text-xl md:text-2xl text-secondary">09:00</h4>
 
-    <div class="hidden md:flex flex-col items-center gap-y-2">
+    <div class="flex flex-col items-center gap-y-2">
       <svg class="w-6 h-6">
         <use
           href="/images/icons.svg#heart"
@@ -21,22 +21,29 @@ import { Star } from '@element-plus/icons-vue';
       />
     </div>
 
-    <div class="md:pb-10 md:group-last:pb-0 space-y-2 md:space-y-4">
-      <h4 class="text-xl md:text-2xl text-primary">Утро невесты</h4>
+    <div class="pb-4 md:pb-8 md:group-last:pb-0 space-y-2 md:space-y-4">
+      <h4 class="text-xl md:text-2xl text-secondary">Утро невесты</h4>
 
       <p class="text-primary-400">Фотосессия в отеле Radisson Collection Hotel</p>
 
       <button
         type="button"
-        class="border-b border-primary-400 hover:border-secondary focus:border-secondary text-primary-400 hover:text-secondary focus:text-secondary text-left transition"
+        class="underline
+               md:no-underline
+               md:border-b
+               border-primary-400
+               hover:border-secondary
+               focus:border-secondary
+               text-primary-400
+               hover:text-secondary
+               focus:text-secondary
+               text-left
+               transition
+        "
       >
         Россия, Москва, Певческий переулок, 4с1
       </button>
     </div>
-
-    <el-icon class="w-6 h-6 group-last:hidden md:!hidden !text-secondary my-4">
-      <star />
-    </el-icon>
   </li>
 </template>
 
